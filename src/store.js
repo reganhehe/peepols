684082311d2cf923127a472a03d45bad24bef3b5
quotidy <48604939+quotidy@users.noreply.github.com>
2019-09-27 21:42:32 -0400

    Add contact page and setup vuex store with basic mutations and state for contact

--- src/store.js
+++ src/store.js
@@ -5,12 +5,30 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    contacts: [
+      {
+        contactName: 'Thomas',
+        nickname: 'Tom',
+        relationship: 'work colleague'
+      },
+      {
+        contactName: 'Regan',
+        nickname: 'Regan',
+        relationship: 'work colleague'
+      },
+    ]
   },
   mutations: {
-
+    addContact: (state, payload) => {
+      state.contacts.push(payload);
+    }
   },
   actions: {
 
+  },
+  getters: {
+    contacts: state => {
+      return state.contacts;
+    }
   }
 })
---
+++ src/views/Contact.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="contact">
+    <h1>This is the contact page</h1>
+    <form>
+      <input name="contactName" type="text" v-model="contactName" placeholder="contactName" />
+      <input name="nickname" type="text" v-model="nickname" placeholder="nickname" />
+      <input name="relationship" type="text" v-model="relationship" placeholder="relationship" />
+    </form>
+    <button @click="addContact">Add new contact</button>
+    <hr />
+    <ul id="contact-information">
+      <li v-for="contact in contacts" :key="contact">
+        Name: {{contact.contactName}}
+        <br />
+        Nickname: {{ contact.nickname }}
+        <br />
+        Relationship: {{ contact.relationship }}
+        <hr />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  data() {
+    return {
+      contactName: "",
+      nickname: "",
+      relationship: ""
+    };
+  },
+  computed: {
+    ...mapGetters({
+      contacts: "contacts"
+    })
+  },
+  methods: {
+    addContact() {
+      this.$store.commit("addContact", {
+        contactName: this.contactName,
+        nickname: this.nickname,
+        relationship: this.relationship
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
